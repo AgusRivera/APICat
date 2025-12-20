a2704770b55159408b894d5a0acc0f841852fdcb
agrivera <agrivera@cnv.gov.ar>
2025-12-20 19:59:26 -0300

    add class configuration EF

---
+++ APICat.Infraestructure/Configurations/BreedConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using APICat.Domain.Entities; // Asegúrate de ajustar este namespace donde tengas la entidad Breed
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace APICat.Infrastructure.Configurations
+{
+    public class BreedConfiguration : IEntityTypeConfiguration<Breed>
+    {
+        public void Configure(EntityTypeBuilder<Breed> builder)
+        {
+            builder.ToTable("Breeds", "dbo");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Name)
+                .HasMaxLength(200)
+                .IsRequired();
+
+            builder.Property(x => x.Origin)
+                .HasMaxLength(200)
+                .IsRequired();
+
+            builder.Property(x => x.Description)
+                .HasMaxLength(200)
+                .IsRequired();
+
+            builder.Property(x => x.Temperament)
+                .HasMaxLength(300)
+                .IsRequired();
+
+            builder.Property(x => x.Wikipedia_url)
+                .HasMaxLength(100)
+                .IsRequired();
+        }
+    }
+}
